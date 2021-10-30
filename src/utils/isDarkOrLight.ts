14573cbd1165205c1946665fbfa07b8909d42203
pryter <peterphongpak@gmail.com>
2021-10-31 06:25:08 +0700

    Fix color issue

--- src/utils/isDarkOrLight.ts
+++ src/utils/isDarkOrLight.ts
@@ -1,26 +1,30 @@
 import { ColorType } from "@components/ColorPicker";
 
+function hexToRgb(hex: string) {
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
 export const isDarkOrLightHEX = (color: string) => {
-  const _color = Array.from(color)
-    .filter((e) => e !== "#")
-    .join("");
 
-  const colorCode =
-    _color.length === 3
-      ? _color
-          .split("")
-          .map((e) => e + e)
-          .join("")
-      : _color;
+  const rgb = hexToRgb(color)
 
-  // @ts-ignore
-  const [a, b, c] = colorCode.matchAll(/[0-9a-fA-F]{2}/gi);
+  if (rgb !== null) {
+    const {r, g, b} = rgb
 
-  if (0.299 * parseInt(a, 16) + 0.587 * parseInt(b, 16) + 0.114 * parseInt(c, 16) > 196) {
-    return "light"; // light
-  } else {
-    return "dark"; // dark
+    if (0.299 * parseInt(r.toString(), 16) + 0.587 * parseInt(g.toString(), 16) + 0.114 * parseInt(b.toString(), 16) > 196) {
+      return "light"; // light
+    } else {
+      return "dark"; // dark
+    }
+  }else{
+    return "dark"
   }
+
 };
 
 export const isDarkOrLightRGBA = (color: ColorType) => {
