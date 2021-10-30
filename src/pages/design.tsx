99eca42b42c2d8729d2adea228e6f0f0c6ad4d90
betich <betich365@gmail.com>
2021-10-30 10:43:57 +0700

    :sparkles: add days

--- src/pages/design.tsx
+++ src/pages/design.tsx
@@ -1,11 +1,44 @@
-import Logo from "@components/Logo";
+import { Logo } from "@components";
 import type { NextPage } from "next";
+import Head from "next/head";
+
+const Mock = {
+  room: "999",
+  program: "ภาษา-ฝรั่งเศส",
+  teachers: ["ครูเรียนเด่น เล่นดี", "ครูทอปเท็น ทอปเท็น"],
+};
+
+const Days = [
+  {
+    name: "จันทร์",
+    color: "#F4CD00",
+  },
+  {
+    name: "อังคาร",
+    color: "#FF92A6",
+  },
+  {
+    name: "พุธ",
+    color: "#ADE374",
+  },
+  {
+    name: "พฤหัสฯ",
+    color: "#FF9417",
+  },
+  {
+    name: "ศุกร์",
+    color: "#45D2F1",
+  },
+];
 
 const Design: NextPage = () => {
   return (
     <>
-      <main className="wrapper">
-        <div className="header">
+      <Head>
+        <title>ตารางเรียน 2/2021</title>
+      </Head>
+      <article className="wrapper">
+        <header className="header">
           <div className="left">
             <div className="bar"></div>
             <div className="title-container">
@@ -14,15 +47,32 @@ const Design: NextPage = () => {
             </div>
           </div>
           <div className="right">
-            <h2 className="room">ม.4 ห้อง 999 | ภาษา-ฝรั่งเศส</h2>
+            <h2 className="room">
+              ห้อง {Mock.room} | {Mock.program}
+            </h2>
             <div className="teacher">
-              <p className="text">ครูเรียนเด่น เล่นดี</p>
-              <p className="text">ครูทอปเท็น ทอปเท็น</p>
+              {Mock.teachers.map((teacher) => (
+                <p className="text" key={teacher}>
+                  {teacher}
+                </p>
+              ))}
             </div>
           </div>
-        </div>
+        </header>
+        <main className="main">
+          <div className="days">
+            {Days.map((day) => (
+              <div className="day" key={day.name}>
+                <div className="button" style={{ backgroundColor: day.color }}>
+                  {day.name}
+                </div>
+                <div className="line" style={{ backgroundColor: day.color }}></div>
+              </div>
+            ))}
+          </div>
+        </main>
         <Logo color="#2B56A8" />
-      </main>
+      </article>
     </>
   );
 };
