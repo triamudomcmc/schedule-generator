b57f57a052ab594f93977eca52374b886ef6666f
betich <betich365@gmail.com>
2021-11-28 19:44:53 +0700

    :art: preview christmas

--- src/components/Background/Mistletoe.tsx
+++ src/components/Background/Mistletoe.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export const MistletoePreview: FC = () => {
   return (
     <div className="w-full h-full absolute top-0 left-0 z-[2]">
-      <Image src="/assets/christmas-bg.png" width={2388} height={1668} />
+      <Image src="/assets/christmas-bg-preview.png" width={2388} height={1668} />
     </div>
   );
 };
