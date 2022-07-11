959f6a40fcb9210cc6c10e8b25570cae922a747b
betich <betich365@gmail.com>
2022-07-12 01:04:58 +0700

    refactor: split page to components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,15 @@ import "@styles/tailwind.scss"
 import "@styles/styles.scss"
 import type { AppProps } from "next/app"
 import { AuthProvider } from "tucmc-auth"
+import { ToastProvider } from "@components/Toast/Context"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <AuthProvider TOKEN="tYjFVdtj_5jlF8GhpZ3QF49moVTNXRM6TzB4axMNADs=">
+    <AuthProvider TOKEN="tYjFVdtj_5jlF8GhpZ3QF49moVTNXRM6TzB4axMNADs=">
+      <ToastProvider>
         <Component {...pageProps} />
-      </AuthProvider>
-    </>
+      </ToastProvider>
+    </AuthProvider>
   )
 }
 export default MyApp
