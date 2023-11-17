f34ef3ac2511ddb87ee5af61ff3a125e89b70777
peter <peterphongpak@gmail.com>
2023-11-17 17:23:01 +0000

    fix: update missing commits

--- src/components/Background/flower.tsx
+++ src/components/Background/flower.tsx
@@ -3,20 +3,19 @@ import { FC } from "react"
 import Image from "next/image"
 
 export const FlowerPreview: FC = () => {
-  return (
-    <div className="absolute top-0 left-0 z-[2] h-full w-full">
-      <Image src="/assets/flower-bg.png" width={2600} height={1816}></Image>
-    </div>
-  )
+    return (
+        <div className="absolute top-0 left-0 z-[2] h-full w-full">
+            <Image src="/assets/flower-bg.png" width={2600} height={1816}></Image>
+        </div>
+    )
 }
 
 const Flower: FC = () => {
-  return (
-    // <div className="absolute top-0 right-0 z-[2] h-[1886px] w-[2700px] ">
-    <div className="absolute top-0 right-0 z-[2] h-[1768px] w-[2388px] ">
-      <Image src="/assets/flower-bg.png" width={2388} height={1768}></Image>
-    </div>
-  )
+    return (
+        <div className="absolute top-0 right-0 z-[2] h-[1886px] w-[2700px] ">
+            <Image src="/assets/flower-bg.png" width={2700} height={1886}></Image>
+        </div>
+    )
 }
 
 export default Flower
